81981661cc194d0755f32cd56cbe770b6961c6e8
Rohith S <rohithofficial10@gmail.com>
2026-03-08 18:00:47 +0530

    Fix:Fixed Navbar layouts

---
+++ frontend/src/app/student/layout.js
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+
+export default function StudentLayout({ children }) {
+  return (
+    <div className="min-h-screen bg-[#F7F9FC]">
+      <Navbar />
+      <main className="pt-8 pb-16">{children}</main>
+    </div>
+  );
+}
