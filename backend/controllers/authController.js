9b8831151ff4f7a684f6403c7116eb069a97d29a
Rohith S <rohithofficial10@gmail.com>
2026-03-23 13:22:45 +0530

    Fix:Fixed env variables for hosting easiness

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,6 +4,8 @@ const { ref } = require('process');
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 const prisma = require('../config/database.js');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const callback = async (req, res) => {
     try {
@@ -40,7 +42,7 @@ const callback = async (req, res) => {
             },
         });
         if (!find) {
-            return res.redirect('http://localhost:3000'); //handle this later to show user doesnt exist and ask them to contact admin
+            return res.redirect(process.env.REDIRECT_URI); //handle this later to show user doesnt exist and ask them to contact admin
         }
         if (find.profilePic.length === 0) {
             await prisma.user.update({
@@ -50,7 +52,7 @@ const callback = async (req, res) => {
         }
         const token = jwt.sign({ userId: find.id, role: find.role }, process.env.JWT_SECRET, { expiresIn: '30m' });
         console.log("User authenticated successfully:", userEmail, token);
-        res.redirect(`http://localhost:3000/login?token=${token}`);
+        res.redirect(`${process.env.REDIRECT_URI}/login?token=${token}`);
     } catch (error) {
         console.error('Error in callback:', error);
         res.status(500).json({ error: 'Internal server error' });
